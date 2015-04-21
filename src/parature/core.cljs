f036be425c00116479188f0445af0cbcac715d4d
Sean Omlor <somlor@eml.cc>
2015-04-21 01:15:51 -0400

    zip up js object from input names/values

--- src/parature/core.cljs
+++ src/parature/core.cljs
@@ -1,97 +1,37 @@
 (ns parature.core
-  (:require [dommy.core :as dommy :refer-macros [sel1]]))
+  (:use [parature.dom :only [attr by-id console-log data domready target]]))
 
-; grrr
-(extend-type js/HTMLCollection
-  ISeqable (-seq [array] (array-seq array 0)))
+(defn input-name [input]
+  (attr input "name"))
 
-(defn click-handler [e]
-  (.log js/console "you clicked!"))
+(defn input-value [input]
+  (.-value input))
 
-(defn console-log [message]
-  (.log js/console message))
+(defn x-inputs [form]
+  (let [inputs (.getElementsByTagName form "input")]
+    (filter #(re-find #"^x_" (str (input-name %))) inputs)))
 
-; (dommy/listen! (sel1 "form[data-etrigue-form]") :click click-handler)
+; map of input name/value pairs
+(defn input-data [inputs]
+  (let [names (map input-name inputs)
+        values (map input-value inputs)]
+    (zipmap names values)))
 
-; (.log js/console "Hello world!")
-
-(defn attr [el key]
-  (.getAttribute el key))
-
-(defn data [el key]
-  (.getAttribute el (str "data-" key)))
-
-(defn target [event] (.-target event))
-
-(defn domready [handler]
-  (.addEventListener js/window "DOMContentLoaded" handler))
-
-(defn by-id [id]
-  (.getElementById js/document (name id)))
-
-(defn extract-inputs [form]
-  (let [inputs (.getElementsByTagName form "input")
-        filtered  (filter #(re-find #"^x_" (str (attr % "name"))) inputs)]
-
-  ; filtered (filter fn [input]
-  ;   (re-matches #"/^x_/" (attr input "name")) inputs)
-
-    ; filtered (filter #(re-matches #"/^x_/" %) inputs)
-
-    ; (.log js/console form)
-    ; (.log js/console (count inputs))
-    ; (.log js/console inputs)
-
-    ; (filter fn [x]
-    ;   (let [name (.getAttribute)]
-
-    ;   )
-    ;   (= ))
-    ;   inputs)
-
-    ; (filter (fn [x]
-    ;   (= (count x) 1))
-    ;   ["a" "aa" "b" "n" "f" "lisp" "clojure" "q" ""])
-
-    (doseq [input filtered]
-      (.log js/console (attr input "name")))))
-
-    ; (doseq [input inputs]
-    ;   (let [name (.getAttribute input "name")]
-    ;     (.log js/console name)))))
+(defn on-submit [selector handler]
+  (.addEventListener (by-id selector) "submit" handler))
 
 (defn submit-handler [event]
+  (console-log "submit handler")
   (.preventDefault event)
-  (.log js/console "submit handler")
-  ; (.log js/console event)
-  ; (.log js/console (target event))
-  (extract-inputs (target event)))
 
-(defn on-submit [selector]
- (.addEventListener (by-id selector) "submit" submit-handler))
+  (let [form (target event)
+        form-id (data form "etrigue-form")
+        inputs (x-inputs form)
+        data (assoc (input-data inputs) "espformid" form-id)]
 
-; (defn on-submit [selector handler]
-;   (let [el (by-id selector)]
-;     (.addEventListener js/window el "submit" handler)))
-
-; (defn watch [event selector handler]
-;   (let [filter-on-selector
-;         (fn [e]
-;           (let [target (target e)
-;                 candidates (.querySelectorAll js/document selector)]
-;             (when (el-in-nodelist candidates target)
-;               (handler e))))]
-;     (.addEventListener js/document event filter-on-selector)))
-
-(defn werd [event]
-  (.preventDefault event)
-  (.log js/console "yep")
-)
+    (console-log (clj->js data))))
 
 (domready
   (fn []
-    ; (.addEventListener (by-id "btn") "click" (fn [] (.log js/console "click")))
-    (.log js/console "dom is ready")
-    (on-submit "hello")
-  )
-)
+    (console-log "dom is ready")
+    (on-submit "etrigue" submit-handler)))
---
+++ src/parature/dom.cljs
@@ -0,0 +1,22 @@
+(ns parature.dom)
+
+; grrr
+(extend-type js/HTMLCollection
+  ISeqable (-seq [array] (array-seq array 0)))
+
+(defn attr [el key]
+  (.getAttribute el key))
+
+(defn by-id [id]
+  (.getElementById js/document (name id)))
+
+(defn console-log [message]
+  (.log js/console message))
+
+(defn data [el key]
+  (.getAttribute el (str "data-" key)))
+
+(defn domready [handler]
+  (.addEventListener js/window "DOMContentLoaded" handler))
+
+(defn target [event] (.-target event))
