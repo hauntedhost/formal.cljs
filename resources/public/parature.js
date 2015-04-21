fdb7d4d73f3b97202a4f4d72c12da2b835c7bd36
Sean Omlor <somlor@eml.cc>
2015-04-21 01:15:38 -0400

    ignore /resources

--- resources/public/parature.js
+++
@@ -1,6 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
-goog.addDependency("../dommy/utils.js", ['dommy.utils'], ['cljs.core']);
-goog.addDependency("../clojure/string.js", ['clojure.string'], ['goog.string', 'cljs.core', 'goog.string.StringBuffer']);
-goog.addDependency("../dommy/core.js", ['dommy.core'], ['cljs.core', 'dommy.utils', 'clojure.string']);
-goog.addDependency("../parature/core.js", ['parature.core'], ['cljs.core', 'dommy.core']);
